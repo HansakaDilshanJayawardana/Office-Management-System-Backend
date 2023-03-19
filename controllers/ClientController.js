f2263be23fa222760823275d45736d62917ea7cf
HansakaDilshanJayawardana <hansaka345@gmail.com>
2023-03-19 21:18:46 +0530

    Update Client Endpoint

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -77,4 +77,35 @@ router.get('/get/:id', async (req, res) => {
     }
 });
 
+// Endpoint for updating an existing client
+router.put('/update/:id', async (req, res) => {
+    try {
+        const client = await Client.findById(req.params.id);
+
+        if (!client) {
+            return res.status(404).json({ msg: 'Client not found' });
+        } else {
+            const { firstName, lastName, gender, nic, address, mobile, email, username } = req.body;
+            const moderator  =  await User.findOne({ 'username': username });
+            const modifiedBy = moderator._id;
+
+            client.firstName = firstName;
+            client.lastName = lastName;
+            client.gender = gender;
+            client.nic = nic;
+            client.address = address;
+            client.mobile = mobile;
+            client.email = email;
+            client.modifiedBy = modifiedBy;
+            client.modifiedAt = Date.now();
+
+            const savedClient = await client.save();
+            res.status(201).json(savedClient);
+        }
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Server Error.'});
+    }
+});
+
 module.exports = router;
